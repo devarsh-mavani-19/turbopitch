0f2b2e9c2990035a222f06b55957b181693e697d
Devarsh Mavani <devarshmavani19@gmail.com>
2022-10-15 19:56:49 +0530

    Bugs Fixd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import WebAgency from "../src/pages/WebAgency";
 import Marketing from "../src/pages/Marketing";
 
 export default function Home() {
   return (
-    <WebAgency />
+    <Marketing />
   )
 }
